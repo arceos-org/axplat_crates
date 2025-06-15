a081ab5f962b213cae4dcccb0e41c2d60faa5d14
equation314 <equation618@gmail.com>
2025-06-15 12:07:25 +0000

    Deploying to gh-pages from @ arceos-org/axplat_crates@6dcbebc9b129dc73eb4c62874ee4a665c5a85a38 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["axplat",["",[],["console.rs","init.rs","irq.rs","lib.rs","mem.rs","power.rs","time.rs"]]],["axplat_aarch64_common",["",[],["generic_timer.rs","gic.rs","lib.rs","pl011.rs","pl031.rs","psci.rs"]]],["axplat_macros",["",[],["lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[91,105,37]}
