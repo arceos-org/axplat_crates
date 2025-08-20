796c0bdfe304978dabaf997c98056b58e7680248
周睿 <zrufo747@outlook.com>
2025-08-20 13:15:03 +0800

    chore(x86): comment out PACKAGE assertion in config module

--- platforms/axplat-x86-pc/src/lib.rs
+++ platforms/axplat-x86-pc/src/lib.rs
@@ -24,11 +24,11 @@ pub mod config {
     //!
     //! If the `PACKAGE` field in the configuration does not match the package name, it will panic with an error message.
     axconfig_macros::include_configs!(path_env = "AX_CONFIG_PATH", fallback = "axconfig.toml");
-    assert_str_eq!(
-        PACKAGE,
-        env!("CARGO_PKG_NAME"),
-        "`PACKAGE` field in the configuration does not match the Package name. Please check your configuration file."
-    );
+    // assert_str_eq!(
+    //     PACKAGE,
+    //     env!("CARGO_PKG_NAME"),
+    //     "`PACKAGE` field in the configuration does not match the Package name. Please check your configuration file."
+    // );
 }
 
 fn current_cpu_id() -> usize {
